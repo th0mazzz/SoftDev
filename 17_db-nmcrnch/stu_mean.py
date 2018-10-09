3f8766540dacb3bd21f79e46b11595dfb1062af0
Thomas-Envy-Laptop <thomas@Thomaz-Envy.localdomain>
2018-10-09 00:23:48 -0400

    Added J-J-J-J-Jack's work

--- 17_db-nmcrnch/stu_mean.py
+++ 17_db-nmcrnch/stu_mean.py
@@ -6,9 +6,7 @@
 import sqlite3   #enable control of an sqlite database
 
 db = sqlite3.connect('dbase.db') #old db with info to use
-avgdb = sqlite3.connect('averages.db') #creating this new one
 c = db.cursor()
-avgc = avgdb.cursor()
 
 idandnames = {} #dict for id and names --> {id1: name1, id2: name2 ...}
 
@@ -33,7 +31,8 @@ def useData(data):
         else: #if id is NOT a key in the dict
             idandgrades[entry[0]] = [entry[3]] #creates key, value pair with list of that grade as the value
             idandnames[entry[0]] = [entry[1]] #adds key, value pair with name as value
-                                    
+        #print(idandgrades)
+        #print(idandnames)
     #print idandgrades
     return idandgrades
 
@@ -52,15 +51,24 @@ def calcAverage(data): #adds up grades in list for each student and divides by l
     
     return data
 
-def doTheTable(tableData):
-     make_avg_table = "CREATE TABLE peeps_avg (id INTEGER PRIMARY KEY, average INTEGER)"
+def addData(tableData):
+    make_avg_table = "CREATE TABLE peeps_avg (id INTEGER PRIMARY KEY, average INTEGER)"
+    c.execute(make_avg_table)
+    for i in tableData:
+        cmd = 'INSERT INTO peeps_avg VALUES(?,?)'
+        c.execute(cmd, (i, tableData[i]))
 
+def addCourse(code, mark, id_num):
+    cmd = 'INSERT INTO courses VALUES(?, ?, ?)'
+    params = (code, mark, id_num)
+    c.execute(cmd, params)
 
 def main():
     grades = extractData()
     inputtedGrades = useData(grades)
     dataForTable = calcAverage(inputtedGrades)
-    doTheTable(dataForTable)
+    addData(dataForTable)
+    addCourse('english', 55, 1)
     
 main()
 
