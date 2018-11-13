d4cb6c2ec52ca8ff1a25f39fe7eac59d1f03d220
th0mazzz <thomaszhao2001@gmail.com>
2018-11-13 17:57:00 -0500

    Finished K24 with Aaron Li's help at WholeFoods

--- 24_rest/app.py
+++ 24_rest/app.py
@@ -3,15 +3,30 @@
 # K24 -- A RESTful Journey Skyward
 # 2018-11-13
 
-#this is your API key: KmUfLJ7wnHIVR9PMe2ha8SlYyWmnKMqYldGqziOp
-#this is their example: https://api.nasa.gov/planetary/apod?api_key=KmUfLJ7wnHIVR9PMe2ha8SlYyWmnKMqYldGqziOp
+#This homework was made possible through the assistance of Aaron Li at the hours of 3:35PM to 5:55PM at the location known as WholeFoods Cafe.
 
-from flask import Flask
+#API key: KmUfLJ7wnHIVR9PMe2ha8SlYyWmnKMqYldGqziOp
+#Example: https://api.nasa.gov/planetary/apod?api_key=KmUfLJ7wnHIVR9PMe2ha8SlYyWmnKMqYldGqziOp
+
+from flask import Flask, render_template
+import json, urllib
+
 app = Flask(__name__)
 
 @app.route("/")
 def root():
-    return "No hablo queso!"
+
+    url="https://api.nasa.gov/planetary/apod?api_key=KmUfLJ7wnHIVR9PMe2ha8SlYyWmnKMqYldGqziOp&date=2018-11-12&hd=True"
+    
+    httpresponse = urllib.request.urlopen(url) #this is initial httpresponse
+    readresponse = httpresponse.read() #we are reading response
+    decodedresponse = readresponse.decode() #we decode it for the json to load later
+    print(readresponse)
+
+    data = json.loads(decodedresponse) #json loads this into usable dictionary
+    
+    
+    return render_template("template.html", data=data)
 
 if __name__ == "__main__":
     app.debug = True
