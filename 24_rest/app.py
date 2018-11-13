9d71266aa6052bdc95020fa4cf0fd7cd12a6eb6c
Thomas <tzhao2@stuy.edu>
2018-11-13 09:59:54 -0500

    Started on K24

---
+++ 24_rest/app.py
@@ -0,0 +1,17 @@
+# Thomas Zhao
+# Softdev pd8
+# K24 -- A RESTful Journey Skyward
+# 2018-11-13
+
+#this is your API key: KmUfLJ7wnHIVR9PMe2ha8SlYyWmnKMqYldGqziOp
+
+from flask import Flask
+app = Flask(__name__)
+
+@app.route("/")
+def root():
+    return "No hablo queso!"
+
+if __name__ == "__main__":
+    app.debug = True
+    app.run()
