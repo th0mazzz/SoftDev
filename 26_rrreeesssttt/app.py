5fe756b4a0d0df6a5820e9e3bf7491d5a642d843
th0mazzz <thomaszhao2001@gmail.com>
2018-11-15 17:04:49 -0500

    Did two APIs

--- 26_rrreeesssttt/app.py
+++ 26_rrreeesssttt/app.py
@@ -9,14 +9,27 @@
 #Example: https://api.nasa.gov/planetary/apod?api_key=KmUfLJ7wnHIVR9PMe2ha8SlYyWmnKMqYldGqziOp
 
 from flask import Flask, render_template
-import json, urllib
+import json, urllib, random
 
 app = Flask(__name__)
 
 ##### Helper Functions #####
 def meme():
-    url="http://version1.api.memegenerator.net//Instances_Search?"
-    apikey = "apikey=34b0833e-2bd7-4614-99d8-cec395f9c68c"
+    url = "http://version1.api.memegenerator.net//Instance_Select?"
+    apikey = "apiKey=9ab0606e-e014-40b3-b9ee-bb8a4d713fa1"
+    instanceid = "&instanceID=" + str(random.randrange(50, 53))
+    fullurl = url + apikey + instanceid
+    
+    httpresponse = urllib.request.urlopen(fullurl) #this is initial httpresponse
+    readresponse = httpresponse.read() #we are reading response
+    decodedresponse = readresponse.decode() #we decode it for the json to load later
+    print(readresponse)
+
+    data = json.loads(decodedresponse) #json loads this into usable dictionary
+    return data
+
+def bored():
+    url="https://www.boredapi.com/api/activity"
     
     httpresponse = urllib.request.urlopen(url) #this is initial httpresponse
     readresponse = httpresponse.read() #we are reading response
@@ -41,9 +54,9 @@ def help_func_name():
 
 @app.route("/")
 def root():
-    nasa_data = nasa()
-    quote_data = quote()
-    return render_template("template.html")
+    meme_data = meme()
+    bored_data = bored()
+    return render_template("template.html", meme_data=meme_data, bored_data=bored_data)
 
 if __name__ == "__main__":
     app.debug = True
