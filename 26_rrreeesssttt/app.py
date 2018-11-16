9c3fefc4c41ea85bd42761a351b5deb90fa4f583
th0mazzz <thomaszhao2001@gmail.com>
2018-11-15 20:07:27 -0500

    Added third (ChuckNorris) API

--- 26_rrreeesssttt/app.py
+++ 26_rrreeesssttt/app.py
@@ -1,6 +1,6 @@
 # Thomas Zhao
 # Softdev pd8
-# K26 -- 
+# K26 -- Getting More REST
 # 2018-11-15
 
 #This homework was made possible through the assistance of Aaron Li at the hours of 3:35PM to 5:55PM at the location known as WholeFoods Cafe.
@@ -39,9 +39,8 @@ def bored():
     data = json.loads(decodedresponse) #json loads this into usable dictionary
     return data
 
-def help_func_name():
-    url=""
-    
+def norris():
+    url="http://api.icndb.com/jokes/random"
     httpresponse = urllib.request.urlopen(url) #this is initial httpresponse
     readresponse = httpresponse.read() #we are reading response
     decodedresponse = readresponse.decode() #we decode it for the json to load later
@@ -56,7 +55,11 @@ def help_func_name():
 def root():
     meme_data = meme()
     bored_data = bored()
-    return render_template("template.html", meme_data=meme_data, bored_data=bored_data)
+    norris_data = norris()
+    return render_template("template.html",
+                           meme_data=meme_data,
+                           bored_data=bored_data,
+                           norris_data=norris_data)
 
 if __name__ == "__main__":
     app.debug = True
